a9e87736144018e8b66464b1034bc6bdb773c581
Colleen Murray <colleenkhm@gmail.com>
2022-06-29 07:01:29 -0500

    add icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
         <footer>
             <ul className="flex-row socials-container">
                 <li>
-                    <a href="https://www.linkedin.com/in/colleen-murray-161314230/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/colleen-murray-161314230/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><i className="fa-solid fa-people-arrows-left-right"></i>LinkedIn</a>
                 </li>
                 <li>
-                    <a href="https://github.com/colleenkhm" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="https://github.com/colleenkhm" target="_blank" rel="noopener noreferrer" title="Github"><i className="fa-solid fa-desktop"></i>GitHub</a>
                 </li>
                 <li>
-                    <a href="https://www.duolingo.com/profile/colleenkhm" target="_blank" rel="noopener noreferrer">Duolingo</a>
+                    <a href="https://www.duolingo.com/profile/colleenkhm" target="_blank" rel="noopener noreferrer" title="Duolingo"><i className="fas fa-comments"></i>Duolingo</a>
                 </li>
             </ul>
         </footer>
